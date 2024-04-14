bb79443ae9ad78c8fc1abc670988da5c1259b269
Lam Nhat Minh <duynghia0705@gmail.com>
2024-04-14 14:49:42 +0700

    update data

--- train_model.py
+++ train_model.py
@@ -5,6 +5,12 @@ from object_detection.utils import config_util
 from object_detection.protos import pipeline_pb2
 from google.protobuf import text_format
 
+
+if tf.config.list_physical_devices('GPU'):
+    physical_devices = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
+    tf.config.experimental.set_virtual_device_configuration(physical_devices[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4000)])
+    
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
 PRETRAINED_MODEL_URL = 'http://download.tensorflow.org/models/object_detection/tf2/20200711/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz'
@@ -13,26 +19,24 @@ LABEL_MAP_NAME = 'label_map.pbtxt'
 MAIN_FOLDER_PATH = os.path.dirname(__file__)
 
 paths = {
-    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
-    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
-    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
-    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
-    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
-    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
-    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
-    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
-    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
-    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
-    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
-    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
+    'WORKSPACE_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join(MAIN_FOLDER_PATH, 'Tensorflow','protoc')
  }
 
-
-
 files = {
-    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
-    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
-    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+    'PIPELINE_CONFIG':os.path.join(MAIN_FOLDER_PATH, 'Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(MAIN_FOLDER_PATH, paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
+    'LABELMAP': os.path.join(MAIN_FOLDER_PATH, paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
 }
 
 # Create Directory Structure
@@ -45,7 +49,7 @@ labels = [
     {'name':'licence', 'id':1}
 ]
 
-with open(MAIN_FOLDER_PATH + "/" + files['LABELMAP'], 'w') as f:
+with open(files['LABELMAP'], 'w') as f:
     for label in labels:
         f.write('item { \n')
         f.write('\tname:\'{}\'\n'.format(label['name']))
@@ -53,32 +57,32 @@ with open(MAIN_FOLDER_PATH + "/" + files['LABELMAP'], 'w') as f:
         f.write('}\n')       
         
 # Create TF records
-command_train = f"python {MAIN_FOLDER_PATH + '/' + files['TF_RECORD_SCRIPT']} -x {MAIN_FOLDER_PATH + '/' + os.path.join(paths['IMAGE_PATH'], 'train')} -l {MAIN_FOLDER_PATH + '/' + files['LABELMAP']} -o {MAIN_FOLDER_PATH + '/' + os.path.join(paths['ANNOTATION_PATH'], 'train.record')}"
-command_test = f"python {MAIN_FOLDER_PATH + '/' + files['TF_RECORD_SCRIPT']} -x {MAIN_FOLDER_PATH + '/' + os.path.join(paths['IMAGE_PATH'], 'test')} -l {MAIN_FOLDER_PATH + '/' + files['LABELMAP']} -o {MAIN_FOLDER_PATH + '/' + os.path.join(paths['ANNOTATION_PATH'], 'test.record')}"
+command_train = f"python {files['TF_RECORD_SCRIPT']} -x {os.path.join(paths['IMAGE_PATH'], 'train')} -l {files['LABELMAP']} -o {os.path.join(paths['ANNOTATION_PATH'], 'train.record')}"
+command_test = f"python {files['TF_RECORD_SCRIPT']} -x {os.path.join(paths['IMAGE_PATH'], 'test')} -l {files['LABELMAP']} -o {os.path.join(paths['ANNOTATION_PATH'], 'test.record')}"
 os.system(command_train)
 os.system(command_test)
 
 # Update config file
-config = config_util.get_configs_from_pipeline_file(MAIN_FOLDER_PATH + "/" + files['PIPELINE_CONFIG'])
+config = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
 pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
-with tf.io.gfile.GFile(MAIN_FOLDER_PATH + "/" + files['PIPELINE_CONFIG'], "r") as f:
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "r") as f:
     proto_str = f.read()                                                                                      
     text_format.Merge(proto_str, pipeline_config)
 
 pipeline_config.model.ssd.num_classes = len(labels)
 pipeline_config.train_config.batch_size = 4
-pipeline_config.train_config.fine_tune_checkpoint = os.path.join(MAIN_FOLDER_PATH, paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
+pipeline_config.train_config.fine_tune_checkpoint = os.path.join(paths['PRETRAINED_MODEL_PATH'], PRETRAINED_MODEL_NAME, 'checkpoint', 'ckpt-0')
 pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
-pipeline_config.train_input_reader.label_map_path = MAIN_FOLDER_PATH + '/' + files['LABELMAP']
-pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(MAIN_FOLDER_PATH, paths['ANNOTATION_PATH'], 'train.record')]
-pipeline_config.eval_input_reader[0].label_map_path = MAIN_FOLDER_PATH + '/' + files['LABELMAP']
-pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(MAIN_FOLDER_PATH, paths['ANNOTATION_PATH'], 'test.record')]
+pipeline_config.train_input_reader.label_map_path = files['LABELMAP']
+pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'train.record')]
+pipeline_config.eval_input_reader[0].label_map_path = files['LABELMAP']
+pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [os.path.join(paths['ANNOTATION_PATH'], 'test.record')]
 config_text = text_format.MessageToString(pipeline_config)
-with tf.io.gfile.GFile(MAIN_FOLDER_PATH + "/" + files['PIPELINE_CONFIG'], "wb") as f:
+with tf.io.gfile.GFile(files['PIPELINE_CONFIG'], "wb") as f:
     f.write(config_text)
     
 #Train model
-TRAINING_SCRIPT = os.path.join(MAIN_FOLDER_PATH, paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
-command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, MAIN_FOLDER_PATH + '/' + paths['CHECKPOINT_PATH'], MAIN_FOLDER_PATH + '/' + files['PIPELINE_CONFIG'])
+TRAINING_SCRIPT = os.path.join(paths['APIMODEL_PATH'], 'research', 'object_detection', 'model_main_tf2.py')
+command = "python {} --model_dir={} --pipeline_config_path={} --num_train_steps=2000".format(TRAINING_SCRIPT, paths['CHECKPOINT_PATH'], files['PIPELINE_CONFIG'])
 
 os.system(command)
